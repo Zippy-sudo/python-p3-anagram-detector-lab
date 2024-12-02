376d5941cfa20dda7761b05e41a08c6fb06c5497
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-12-03 00:47:25 +0300

    First Trial

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,24 @@
-# your code goes here!
+import ipdb
+class Anagram:
+
+    def __init__(self, word="sets"):
+        self.word = word
+
+    def set_word(self):
+        return self._word 
+    
+    def get_word(self, word):
+        self._word = word
+
+    word = property(set_word, get_word)
+
+    def match(self, potential_anagrams):
+        anagrams = []
+        for potential_anagram in potential_anagrams:
+            if sorted(list(potential_anagram)) == sorted(list(self._word)):
+                anagrams.append(potential_anagram)
+        return anagrams
+
+
+sets = Anagram("sets")
+print(sets.match(["sets" , "chicken", "etss"]))
